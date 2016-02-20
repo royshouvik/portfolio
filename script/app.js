d223147915fe2f7b42c7cec34b7411ff515d802b
Shouvik Roy <shouvik@techie.com>
2016-02-21 02:01:38 +0530

    Added svg and scroll

--- script/app.js
+++ script/app.js
@@ -1,40 +1,37 @@
+(function() {
+                // initialize svg
+
+                [].slice.call( document.querySelectorAll( '.si-icons-default > .si-icon' ) ).forEach( function( el ) {
+                    var svgicon = new svgIcon( el, svgIconConfig );
+                } );
+                new svgIcon( document.querySelector( '.si-icons-easing .si-icon-hamburger-cross' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
+
+            })();
+
+
 $(document).ready(function() {
-    $("ul.nav li").click(function(event) {
-        $("li").removeClass("active");
-        $(this).addClass("active");
-        event.preventDefault();
+    $('#fullpage').fullpage({
+        menu: '#navmenu',
+        autoScrolling: false,
+        anchors:['intro', 'portfolio', 'about', 'contact'],
+        resize : false,
+        fitToSection : false,
+        normalScrollElements: '.footer',
+        afterLoad: function () {
+            console.log("hello");
+            $("nav").removeClass("display");
+        }
     });
 
-    $("#portfolio-button").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".portfolio").offset().top
-        }, 500);
-    });
+    var hideNav = function () {
+        console.log("hello");
+        $("nav").removeClass("display");
+    }
 
-    $("#intro-button").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".intro").offset().top
-        }, 500);
-    });
-
-    $("#about-button").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".about").offset().top
-        }, 500);
-    });
-
-    $("#contact-button").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".contact").offset().top
-        }, 500);
-    });
 
     $(".toggle").click(function(event) {
         $("nav").toggleClass("display");
         event.preventDefault();
     });
-
-
-
 
 });
